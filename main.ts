5b76ea7cf34d286034a8d1e7ee0bea1468769904
LeaHerrera <146827129+LeaHerrera@users.noreply.github.com>
2024-11-04 18:44:32 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("\"Holaa gente!\"")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString("" + (radio.receivedPacket(RadioPacketProperty.SignalStrength)))
+})
 basic.forever(function () {
 	
 })
